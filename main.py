c891a3790ef04a30051d122fdbd19dcc7b36d18f
Matt Kelly <matt.kelly@ventistal.no>
2021-03-13 20:39:30 +0100

    started FastAPI Web Apps by Talk python

--- main.py
+++ main.py
@@ -1,1 +1,17 @@
 import fastapi
+import uvicorn
+
+app = fastapi.FastAPI()
+
+
+@app.get('/')
+def index():
+    return {
+        "message: Hello World"
+    }
+
+
+if __name__ == '__main__':
+    uvicorn.run(app)
+
+
